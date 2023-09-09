398743d8de6ee29c81d32bf05b8059b784a198ed
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-09-09 10:04:20 +0900

    week03 chap01

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,4 @@
+fahrenheit = input("input temperature in fahrenheit : ")
+# (100°F − 32) × 5/9 = 37.778°C
+celsius = (int(fahrenheit) - 32) * 5 / 9
+print(f"Fahrenheit {fahrenheit}F equal to Celsius {celsius}C.")
