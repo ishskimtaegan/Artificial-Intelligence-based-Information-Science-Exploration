2f8a93dbc59b411cc735eb8f3b2c2ae5cb388981
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 10:52:10 +0900

    week05 chap06 v2.3

--- week05_chap06.py
+++ week05_chap06.py
@@ -1,3 +1,13 @@
-dan = int(input("단 입력 : "))
-for i in range(1, 10):
-    print(f"{dan} * {i} = {dan*i}")
+# dan = int(input("단 입력 : "))
+# for i in range(1, 10):
+#     print(f"{dan} * {i} = {dan*i}")
+#
+# print(list(range(1, 10)))
+
+# count = 5
+# while count > 0:
+#     print(count)
+#     count = count - 1  # count -= 1
+
+for i in range(5, 0, -1): # 5부터 0보다 클 때까지 -1씩 감소
+    print(i)
