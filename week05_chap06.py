988cb94ec6ac9bf6b81564566070364372cca608
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 10:46:00 +0900

    week05 chap06 v2.2

--- week05_chap06.py
+++ week05_chap06.py
@@ -1,7 +1,3 @@
-texts = 'ishs'
-for text in texts:
-    if text == 'q':
-        break
-    print(text.upper())
-else:
-    print("알파벳 소문자 q를 찾지 못했습니다")
+dan = int(input("단 입력 : "))
+for i in range(1, 10):
+    print(f"{dan} * {i} = {dan*i}")
