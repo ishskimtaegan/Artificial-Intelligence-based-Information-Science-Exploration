883aac99d8d5cd15fce725f55e8038bb794ea7d2
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 11:47:30 +0900

    week05 chap07 v2.9

--- week05_chap07.py
+++ week05_chap07.py
@@ -1,12 +1,9 @@
-# scores = (100, 98, 99)
-# print(scores)
-# scores[1] = 99  # 'tuple' object does not support item assignment
-# print(scores)
+l1 = []
+l2 = list()
+if l1 == l2:
+    print("empty lists")
+    print(type(l1), type(l2))
+    print(l1, l2)
 
-scores = (100, 98, 99)
-print(scores)
-scores_list = list(scores)  # 리스트로 변환
-print(scores_list)
-scores_list[1] = 99  # 리스트 상태에서 수정 후
-scores = tuple(scores_list)  # 튜플로 변환
-print(scores)
+l3 = 'ishs'
+print(list(l3))
