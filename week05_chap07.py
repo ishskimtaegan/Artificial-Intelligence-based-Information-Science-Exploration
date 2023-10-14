c700ab993351b5dbe3e571f15ea80af25572dac6
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 11:25:50 +0900

    week05 chap07 v2.6

--- week05_chap07.py
+++ week05_chap07.py
@@ -1,15 +1,8 @@
-t1 = ()
-t2 = ('ishs') # str
-t3 = 'ishs',
-t4 = ('ishs',)
-t5 = ('ishs', 'python', 'java')
-t6 = 'ishs', 'python', 'java'
-print(type(t1))
-print(type(t2))
-print(type(t3))
-print(type(t4))
-print(type(t5))
-print(t5[1])  # same as list indexing
-print(type(t6))
-print(type('test',))  # str
-print(type(('test',)))  # tuple
+subjects = ('python', 'english', 'math')  # packing
+print(subjects)
+print(type(subjects))
+print(subjects[1])
+#s1, s2, s3, s4 = subjects  # not enough values to unpack (expected 4, got 3)
+#s1, s2 = subjects  # too many values to unpack (expected 2)
+s1, s2, s3 = subjects  # unpacking
+print(s2)
