67d99ab9829ff99ed13647a0aa13cd93fb0a1c28
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 10:36:04 +0900

    week05 chap06 v1.9

--- week05_chap06.py
+++ week05_chap06.py
@@ -1,12 +1,7 @@
-index = 0
-numbers = [9, 0, 11, -3, 55, 0]
-# print(numbers[4])  # 55
-while index < len(numbers):
-    number = numbers[index]
-    if number < 0:
-        print('음수 발견!', number)
-        break
-    print(index)
-    index = index + 1
-else:
-    print('음수를 찾지 못했습니다')
+texts = 'ishs'
+# index = 0
+# while index < len(texts):
+#     print(texts[index].upper())
+#     index = index + 1
+for text in texts:
+    print(text.upper())
