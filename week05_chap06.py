40d13e5e74d9098b720e92f7c198b71e19f65a8e
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 10:27:33 +0900

    week05 chap06 v1.8

--- week05_chap06.py
+++ week05_chap06.py
@@ -1,16 +1,12 @@
-while True:
-    menu = input("1) 온도변환(화씨->섭씨)   2) 절대값   3) 종료 : ")
-    if menu == '1':
-        fahrenheit = input("화씨 온도 입력 : ")
-        # (100°F − 32) × 5/9 = 37.778°C
-        celsius = (float(fahrenheit) - 32) * 5 / 9
-        print(f"화씨 {fahrenheit}도는 섭씨 {celsius}도 입니다.")
-    elif menu == '2':
-        number = int(input("정수 입력 : "))
-        if number > 0:
-            continue
-        print(f"음수 {number}의 절대값은 {abs(number)}입니다")
-    elif menu == '3':
+index = 0
+numbers = [9, 0, 11, -3, 55, 0]
+# print(numbers[4])  # 55
+while index < len(numbers):
+    number = numbers[index]
+    if number < 0:
+        print('음수 발견!', number)
         break
-    else:
-        print("메뉴에서 골라주세요.")
+    print(index)
+    index = index + 1
+else:
+    print('음수를 찾지 못했습니다')
