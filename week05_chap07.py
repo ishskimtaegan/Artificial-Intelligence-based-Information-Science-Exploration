eef5e979365aac234e44cb1c2d927c0259c5a716
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 11:41:15 +0900

    week05 chap07 v2.8

--- week05_chap07.py
+++ week05_chap07.py
@@ -1,9 +1,12 @@
-# favor_subjects = ('python', 'english', 'math')
-# hate_subjects = ('history', 'music')
-# print(favor_subjects + hate_subjects)
-# print(favor_subjects)
-favor_subjects = ('python', 'english', 'math')
-hate_subjects = ('history', 'music')
-print(favor_subjects)
-favor_subjects = favor_subjects + hate_subjects  # concatenate (combine) tuples
-print(favor_subjects)
+# scores = (100, 98, 99)
+# print(scores)
+# scores[1] = 99  # 'tuple' object does not support item assignment
+# print(scores)
+
+scores = (100, 98, 99)
+print(scores)
+scores_list = list(scores)  # 리스트로 변환
+print(scores_list)
+scores_list[1] = 99  # 리스트 상태에서 수정 후
+scores = tuple(scores_list)  # 튜플로 변환
+print(scores)
