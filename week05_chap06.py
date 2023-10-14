877b34184bce80e00f3ad0a6923da0708ac3513d
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-10-14 09:42:21 +0900

    week05 chap06 v1.6

--- week05_chap06.py
+++ week05_chap06.py
@@ -1,4 +1,12 @@
-count = 10
-while count > 0:
-    print(count)
-    count = count - 1  # count -= 1
+while True:
+    menu = input("1) 온도변환(화씨->섭씨)   2) 종료 : ")
+    if menu == '1':
+        fahrenheit = input("화씨 온도 입력 : ")
+        # (100°F − 32) × 5/9 = 37.778°C
+        celsius = (float(fahrenheit) - 32) * 5 / 9
+        print(f"화씨 {fahrenheit}도는 섭씨 {celsius}도 입니다.")
+    elif menu == '2':
+        break
+    else:
+        print("메뉴에서 골라주세요.")
+
