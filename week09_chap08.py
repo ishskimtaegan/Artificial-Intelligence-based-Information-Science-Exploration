2600648c44e80aa850663ec520522954d42ae8c0
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-11-18 10:31:43 +0900

    week09 chap08 v4.8

--- week09_chap08.py
+++ week09_chap08.py
@@ -1,29 +1,8 @@
-#subjects = dict(python=5, java=9)  # subjects = {'python': 5, 'java': 9}
-subjects = [['python', 5], ['java', 9]]  # 2d list
+subjects = [['python', 5], ['java', 9], ['go', 7]]  # 2d list
 subjects = dict(subjects)  # transform 2d list to dictionary
 
-print(subjects)
-print(subjects['java'])  # access
-subjects['go'] = 7  # add
-print(subjects)
-subjects['java'] = 8  # update
-print(subjects)
-
-for subject in subjects:  # print key values
-    print(subject)
-
-for subject in subjects.values():  # print values
-    print(subject)
-
-for subject in subjects.keys():  # print key values
-    print(subject)
-
-for subject in subjects.items():  # print key and values in tuple form, (key, value)
-    print(subject)
-
-for subject, students in subjects.items():  # unpacking tuples
-    #print(subject, students)
-    print(f"{subject} 과목을 수강하는 학생의 인원은 {students}명 입니다")
-
-del subjects['java']  # delete
-print(subjects)
+print(subjects['go'])
+print(subjects.get('go'))
+print(subjects.values())
+print(list(subjects.values()))
+print(len(subjects))
