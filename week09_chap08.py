77f8ea865b6aff0379c6e4b861356fed2ab15df1
ishs.kimtaegan <ishs.kimtaegan@gmail.com>
2023-11-18 10:38:17 +0900

    week09 chap08 v4.9

--- week09_chap08.py
+++ week09_chap08.py
@@ -1,8 +1,13 @@
-subjects = [['python', 5], ['java', 9], ['go', 7]]  # 2d list
-subjects = dict(subjects)  # transform 2d list to dictionary
-
-print(subjects['go'])
-print(subjects.get('go'))
-print(subjects.values())
-print(list(subjects.values()))
-print(len(subjects))
+subjects1 = {'python': 5, 'java': 9}
+subjects2 = {'data structure': 5, 'java': 9, 'go': [7, 8]}
+subjects3 = subjects2
+subjects4 = dict(subjects2)
+subjects = {**subjects1, **subjects2}
+print(subjects)
+subjects2['java'] = 11
+subjects2['go'][0] = 12
+print(subjects)
+print(subjects1)
+print(subjects2)
+print(subjects3)
+print(subjects4)
